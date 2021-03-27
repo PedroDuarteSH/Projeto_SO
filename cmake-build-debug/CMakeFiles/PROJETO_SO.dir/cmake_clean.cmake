9cb6c9ae17935b4a6d04610e6fde05145f1597e3
Duarte Henriques <78002605+PedroDuarteSH@users.noreply.github.com>
2021-03-27 15:51:14 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/PROJETO_SO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROJETO_SO.dir/main.c.o"
+  "PROJETO_SO.exe"
+  "PROJETO_SO.pdb"
+  "libPROJETO_SO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PROJETO_SO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PROJETO_SO.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PROJETO_SO.
+# This may be replaced when dependencies are built.
