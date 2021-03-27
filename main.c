8a225914eedd1b542b8cb5ab7f71db23a6eafd17
Duarte Henriques <78002605+PedroDuarteSH@users.noreply.github.com>
2021-03-27 16:26:38 +0000

    Update main.c

--- main.c
+++ main.c
@@ -4,5 +4,9 @@
 int main() {
     printf("Hello 123, World!\n");
     printf("asdasda");
+    printf("asdasda");
+    printf("asdasda");
+    printf("asdasda");
+    printf("asdasda");
     return 0;
 }
