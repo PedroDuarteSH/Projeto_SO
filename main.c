f7791a907fa0424be61b66c97404a796776dfb88
PedroDuarteSH <pedroduartesh@gmail.com>
2021-03-27 17:35:20 +0000

    a

--- main.c
+++ main.c
@@ -8,14 +8,10 @@ int main() {
     printf("Hello 123, World!\n");
     printf("asdasda\n");
     printf("ola\n");
-<<<<<<< HEAD
     printf("ola");
     printf("ok");
-=======
     int a = 12;
     printf("%d", a);
     zeca();
->>>>>>> Duarte
     return 0;
 }
-
