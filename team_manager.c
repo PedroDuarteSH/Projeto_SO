def33bad1e07fc96ddf27119f03412770238704c
Duarte Henriques <pedroduartesh@gmail.com>
2021-03-27 19:08:04 +0000

    begin

---
+++ team_manager.c
@@ -0,0 +1,1 @@
+//Processo responsável pela gestão da box e dos carros da equipa. É ainda responsável pela reparação dos carros da equipa e por atestar o depósito de combustível. Existe um por equipa.
