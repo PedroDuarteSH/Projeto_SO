94555ec2382bf301912fea0f29c0f554cbc3531f
joaojoaofernandes <72656068+joaojoaofernandes@users.noreply.github.com>
2021-03-28 16:52:43 +0100

    Functions file (#8)
    * Acrescento do functions.c e function.h
    
    * é desta
    
    * added Strip function
    
    * ta bom?

--- race_simulator.c
+++ race_simulator.c
@@ -1,4 +1,5 @@
 #include "race_simulator.h"
+#include "functions.c"
 
 //Main file
 //Processo  responsável  por  iniciar  o sistema e os restantes processos do simulador.
@@ -26,46 +27,4 @@ int main() {
     if(configs == NULL)
         printf("Error reading file or invalid number of teams\ncheck if your file is config.txt or the number of teams (line 3) is bigger than 3!");
     
-}
-
-int *read_config_file(){
-    /* Config int[] format
-    0 - T_units_second;
-    1 - lap_distance;
-    2 - lap_number;
-    3 - number_of_teams (must be > 3);
-    4 - max_cars_team;
-    5 - T_breakdown_interval;
-    6 - T_Box_min;
-    7 - T_Box_Max;
-    8 - Fuel_tank_capacity;
-    */
-    FILE *conf_file = fopen("config.txt", "r");
-
-    if(conf_file == NULL)
-        return NULL;
-    
-    
-    int *configs = malloc(sizeof(int) * number_of_configs);
-    int current_index = 0;
-
-    char *line = malloc(sizeof(char) * line_lenght);
-    char *temp_strtok;
-    char *elem;
-    while(fgets(line,line_lenght, conf_file) != NULL){
-        strip(line);
-        elem = strtok(line, ",");
-        configs[current_index++] = strtol(elem, &temp_strtok, 10);
-        if((elem = strtok(NULL, ",")) != NULL)
-            configs[current_index++] = strtol(elem, &temp_strtok, 10);
-    }
-    if(configs[3] < 3)
-        return NULL;
-    return configs;
-} 
-
-
-void strip(char *phrase){
-    phrase[strcspn(phrase, "\r")] = 0;
-    phrase[strcspn(phrase, "\n")] = 0;
 }
