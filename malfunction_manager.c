d8dc3f41c35e3bfa2f3388981002bc9563540ee5
Duarte Henriques <pedroduartesh@gmail.com>
2021-03-30 00:07:20 +0100

    ola

--- malfunction_manager.c
+++ malfunction_manager.c
@@ -1,3 +1,7 @@
 //Processo  responsável  por  gerar aleatoriamente as avarias dos carros, a partir da informação da sua fiabilidade
 //Produz avarias randomicamente Comunica por Message queque
-void malfunction_manager(){}
+void malfunction_manager(){
+
+
+    
+}
--- race_manager.c
+++ race_manager.c
@@ -9,17 +9,19 @@ shr_memory *shm_struct;
 config *config_struct;
 race *race_struct;
 team **teams;
+
 #define TRUE 1
 #define FALSE 0
+
 //temporary
 #define INPUT_LENGHT 200
-
+#define SMALL_STR_LENGHT 10
 void race_manager_init(int incoming_shm_id){
     attach_update_shm(incoming_shm_id);
-    
-    #ifdef debug
+
+#ifdef debug
     print_config_file();
-    #endif
+#endif
 
     //avisar o semaforo que a corrida
     //ler o ficheiro / Receber namedPipe
@@ -27,34 +29,40 @@ void race_manager_init(int incoming_shm_id){
 
     char *line = malloc(sizeof(char) * INPUT_LENGHT);
     char *command = malloc(sizeof(char) * INPUT_LENGHT);
-
-    while(fgets(line, INPUT_LENGHT, cars_file) != NULL){
-        //sem_getvalue -> SABER VALOR DO SEMAFORO
-        strip(line);
-        strcpy(command, line);
-        command = strtok(command, " ");
-
-        if(strcmp(command, "START RACE!") == 0){
-            if(verify_teams() == TRUE){
-
+    int race_started = FALSE;
+    while (TRUE){
+        line[0] = 0;
+        fgets(line, INPUT_LENGHT, cars_file);
+        if (race_started == FALSE){
+            strip(line);
+            strcpy(command, line);
+            command = strtok(command, " ");
+            if (strcmp(line, "START RACE!") == 0){
+                if (verify_teams() == TRUE){
+                    race_started = TRUE;
+                    start_race();
+                    sem_post(&race_struct->race_begin);
+                }
+                else print("CANNOT START, NOT ENOUGH TEAMS");
             }
-
-            
-            
-            
+            else if (strcmp(command, "ADDCAR") == 0){
+                print(line);
+                sleep(1);
+            }
+            else
+                print(strcat("WRONG COMMAND => ", line));
+        
+        }
+        else{
+            if(line[0] != 0) //invalid command
+                print("Rejected, race already started!");
             break;
         }
-        else if(strcmp(command, "ADDCAR") ==0){ 
-            
-            sleep(1);
-        }
-        else
-            print(strcat("WRONG COMMAND => ", line));        
     }
     free(command);
-    print("Race Started");
-    sem_post(&race_struct->race_begin);
     
+    
+
     //Espera que o setup das equipas esteja feito
     //Começa a Corrida
     //pid_t new_team;
@@ -66,13 +74,11 @@ void race_manager_init(int incoming_shm_id){
           team_manager();
        }
     }*/
-
-
-
 }
 
-void print_config_file(){
-    printf("%d\n",config_struct->T_units_second);
+void print_config_file()
+{
+    printf("%d\n", config_struct->T_units_second);
     printf("%d\n", config_struct->lap_distance);
     printf("%d\n", config_struct->lap_number);
     printf("%d\n", config_struct->number_of_teams);
@@ -83,14 +89,16 @@ void print_config_file(){
     printf("%d\n", config_struct->Fuel_tank_capacity);
 }
 
-void attach_update_shm(int incoming_shm_id){
+void attach_update_shm(int incoming_shm_id)
+{
     //first 3 lines wasn't needed (already attached in father process)
     shm_id = incoming_shm_id;
     shm_struct = shmat(shm_id, NULL, 0);
     config_struct = shmat(shm_struct->config_shmid, NULL, 0);
     race_struct = shmat(shm_struct->race_shmid, NULL, 0);
     //iniciar equipas
-    if ((race_struct->teams_shmid = shmget(IPC_PRIVATE, sizeof(team *) * config_struct->number_of_teams, IPC_CREAT | 0777)) < 1){
+    if ((race_struct->teams_shmid = shmget(IPC_PRIVATE, sizeof(team *) * config_struct->number_of_teams, IPC_CREAT | 0777)) < 1)
+    {
         print("Error in shmget with IPC_CREAT creating teams array");
         exit(1);
     }
@@ -98,19 +106,22 @@ void attach_update_shm(int incoming_shm_id){
     teams = shmat(race_struct->teams_shmid, NULL, 0);
     for (int i = 0; i < config_struct->number_of_teams; i++)
         teams[i] = NULL;
-    
-    
 }
 
-team * find_key(){
-
-
-
+void start_race(){
 
 }
 
+team *find_team(char *team){
+    int consulted_teams = 0;
+
+    
+}
 
 car *ADD_CAR(char *line){
+    char *team = malloc(sizeof(char) * SMALL_STR_LENGHT);
+    strtok(line, " ");//First 
+
 
 
 
@@ -118,8 +129,8 @@ car *ADD_CAR(char *line){
 
 int verify_teams(){
     for (int i = 0; i < config_struct->number_of_teams; i++){
-        if(teams[i] == NULL)
-                    return FALSE;
-    return TRUE;
+        if (teams[i] == NULL)
+            return TRUE;
+        return TRUE;
+    }
 }
-
