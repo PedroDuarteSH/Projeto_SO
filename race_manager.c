ec9e521a330ec7a13161d2f98afd14f7eeb36c79
Duarte Henriques <pedroduartesh@gmail.com>
2021-03-30 15:29:47 +0100

    las

--- race_manager.c
+++ race_manager.c
@@ -3,6 +3,8 @@
 //Cria processos Team_manager
 #include "race_manager.h"
 
+char* itoa(int value, char* result, int base);
+
 //initiated in father process
 int shm_id;
 shr_memory *shm_struct;
@@ -39,7 +41,6 @@ void race_manager_init(int incoming_shm_id){
             sleep(1);
         }
         else{
-            sem_post(&race_struct->race_begin);
             if(line[0] != 0) //invalid command
                 print("Rejected, race already started!");
             break;
@@ -92,7 +93,7 @@ void attach_update_shm(int incoming_shm_id){
 }
 
 void start_race(){
-
+    sem_post(&race_struct->race_begin);
 }
 
 team *find_team(char *team_name){
@@ -113,28 +114,26 @@ team *find_team(char *team_name){
             teams[i]->number_team_cars = 0;
             return teams[i];
         }
-        else if(strcmp(teams[i]->name, team_name) == 0)
-            return teams[i];
-        
+        else if(strcmp(teams[i]->name, team_name) == 0) return teams[i];   
     }
     return NULL;
 }
 
-int add_car(char *line){
+void add_car(char *line){
     char *temp = concat("", line);
     char **line_splited = malloc(sizeof(char *) * team_input_size);
     
     if(verify_car_command(temp, line_splited) == FALSE){
         print(concat("WRONG COMMAND => ", line));
-        return 0; //Wrong command
+        return; //Wrong command
     }
-
+    free(temp);
     team *t;
     if((t = find_team(line_splited[2])) == NULL){
         #ifdef debug
         print(concat("ERROR FINDING TEAM =>", line));
-        #endif
-        return 1; //Unable to find the team
+        #endif//Unable to find the team
+        return;
     }
     int car_shmid;
     if ((car_shmid = shmget(IPC_PRIVATE, sizeof(car) * config_struct->max_cars_team, IPC_CREAT | 0777)) < 1){
@@ -150,20 +149,17 @@ int add_car(char *line){
     c->consumption = strtof(line_splited[8], &temp);
     c->reliability = strtol(line_splited[10], &temp, 10);
     team_cars[t->number_team_cars++] = c;
-
-    
-    print(concat("CAR ADDED SUCCESSFULLY => ", line));
-
     shmdt(team_cars);
     shmdt(c);
-
-    return 2; //Car added successfully
+    print(concat("CAR ADDED SUCCESSFULLY => ", line));
+    //Car added successfully
 }
 
 int verify_teams(){
-    for (int i = 0; i < config_struct->number_of_teams; i++)
+    for (int i = 0; i < config_struct->number_of_teams; i++){
         if (teams[i] == NULL)
             return FALSE;
+    }
     return TRUE;
 }
 
@@ -171,13 +167,16 @@ int verify_car_command(char *line, char ** line_splited){
     for (int i = 0; i < team_input_size; i++)
         line_splited[i] = NULL;
     //read command
-    line_splited[0] = strtok(line, ", \n");
-    for (int i = 1; i < team_input_size; i++)
-        line_splited[i] = strtok(NULL, ", \n");
+    char* temp;
+    char* saveptr = line;
+    int index = 0;
+    while((temp = strtok_r(saveptr, " ,\n", &saveptr))){
+        line_splited[index] = concat("", temp);
+        index++;
+    }
     //verify if something is NULL
-    for (int i = 1; i < 11; i++)
-        if(line_splited[i] == NULL)
-            return FALSE;
+    if(line_splited[team_input_size-1] == NULL)
+        return FALSE;
     return TRUE;
 }
 
@@ -202,3 +201,26 @@ int process_command(char *line){
     return FALSE;
 }
 
+char* itoa(int value, char* result, int base) {
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
