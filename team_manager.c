fcd97d8fb840172d701faf0f2911ec3ba00a487e
Duarte Henriques <78002605+PedroDuarteSH@users.noreply.github.com>
2021-04-05 14:41:51 +0100

    Update team_manager.c

--- team_manager.c
+++ team_manager.c
@@ -1,3 +1,6 @@
+//Joao Fernandes 2019220273
+//Pedro Henriques 2019217793
+
 //Processo responsável pela gestão da box e dos carros da equipa. É ainda responsável pela reparação dos carros da equipa e por atestar o depósito de combustível. Existe um por equipa.
 //Fazer a Gestão da Box (APENAS 1) e Atualiza-a (Livre, Ocupada, reservada)
 //Fazer a Gestão dos Carros da equipa
